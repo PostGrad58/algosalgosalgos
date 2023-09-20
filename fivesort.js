8d49786884e38b7f654919e0ea6ec8f193504808
Daniel Park <danielpark@Daniels-MacBook-Air.local>
2023-09-20 11:35:42 -0700

    adds fivesort solution

--- fivesort.js
+++ fivesort.js
@@ -1,4 +1,6 @@
-// Write a function, fiveSort, that takes in an array of numbers as an argument. The function should rearrange elements of the array such that all 5s appear at the end. Your function should perform this operation in-place by mutating the original array. The function should return the array.
+// Write a function, fiveSort, that takes in an array of numbers as an argument. The function should rearrange elements of the array 
+// such that all 5s appear at the end. Your function should perform this operation in-place by mutating the original array. 
+// The function should return the array.
 
 // Elements that are not 5 can appear in any order in the output, as long as all 5s are at the end of the array.
 
@@ -11,7 +13,7 @@
 // fiveSort([5, 5, 6, 5, 5, 5, 5]);
 // // -> [6, 5, 5, 5, 5, 5, 5] 
 // fiveSort([5, 1, 2, 5, 5, 3, 2, 5, 1, 5, 5, 5, 4, 5]);
-// // -> [4, 1, 2, 1, 2, 3, 5, 5, 5, 5, 5, 5, 5, 5] 
+// // -> [4, 1, 2, 1, 2, 3, 5, 5, 5, 5, 5, 5, 5, 5]
 // const fives = new Array(20000).fill(5);
 // const fours = new Array(20000).fill(4);
 // const nums = [...fives, ...fours];
@@ -19,3 +21,31 @@
 // // twenty-thousand 4s followed by twenty-thousand 5s
 // // -> [4, 4, 4, 4, ..., 5, 5, 5, 5]
 
+function fiveSort(numsArr) {
+    let leftPtr = 0;
+    let rightPtr = numsArr.length - 1;
+
+    while (leftPtr < rightPtr) {
+        while (numsArr[leftPtr] !== 5) {
+            leftPtr++;
+        }
+        while (numsArr[rightPtr] === 5) {
+            rightPtr--;
+        }
+
+        let temp = numsArr[leftPtr];
+        numsArr[leftPtr] = numsArr[rightPtr];
+        numsArr[rightPtr] = temp;
+
+        leftPtr++;
+        rightPtr--;
+    }
+
+    return numsArr;
+}
+
+// console.log(fiveSort([12, 5, 1, 5, 12, 7]));
+console.log(fiveSort([5, 2, 5, 6, 5, 1, 10, 2, 5, 5]));
+// console.log(fiveSort([5, 5, 5, 1, 1, 1, 4]));
+// console.log(fiveSort([5, 5, 6, 5, 5, 5, 5]));
+// console.log(fiveSort([5, 1, 2, 5, 5, 3, 2, 5, 1, 5, 5, 5, 4, 5]));
