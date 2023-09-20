0b447d84c52504b53f72257c26cbef76c1eb21a1
fbiswas <fpena213@gmail.com>
2023-09-20 09:34:30 -0700

    solves intersection

---
+++ intersection.js
@@ -0,0 +1,41 @@
+// Write a function, intersection, that takes in two arrays, a,b, as arguments. The function should return a new array containing elements that are in both of the two arrays.
+
+// You may assume that each input array does not contain duplicate elements.
+
+// intersection([4,2,1,6], [3,6,9,2,10]) // -> [2,6]
+// intersection([2,4,6], [4,2]) // -> [2,4]
+// intersection([4,2,1], [1,2,4,6]) // -> [1,2,4]
+// intersection([0,1,2], [10,11]) // -> []
+// const a = [];
+// const b = [];
+// for (let i = 0; i < 50000; i += 1) {
+//   a.push(i);
+//   b.push(i);
+// }
+// intersection(a, b) // -> [0,1,2,3,..., 49999]
+
+const intersection = (a, b) => {
+    //i: 2 arrays
+    //0: new array
+    const set = new Set(a);
+    const arr = [];
+    b.forEach((e) => {
+      if (set.has(e)) arr.push(e) 
+    })
+    return arr;
+    
+    // const cache = {};
+    // const arr = []
+    // a.forEach((e) => {
+    //   cache[e] ? cache[e]++ : cache[e] = 1;
+    // })
+    // b.forEach((e) => {
+    //   cache[e] ? cache[e]++ : cache[e] = 1;
+    // })
+    // for (let key in cache) {
+    //   if (cache[key] === 2) {
+    //     arr.push(Number(key))
+    //   }
+    // }
+    // return arr;
+  };
